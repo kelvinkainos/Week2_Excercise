2ec5ae2e27ee63876a3cffe0de097277b6cff386
SeanMcAvoy29 <sean.mcavoy@Kainos.com>
2023-08-24 15:32:03 +0100

    fixed comments from pull request

--- src/main/java/org/kainos/ea/client/EmployeeDoesNotExistException.java
+++ src/main/java/org/kainos/ea/client/EmployeeDoesNotExistException.java
@@ -3,6 +3,6 @@ package org.kainos.ea.client;
 public class EmployeeDoesNotExistException extends Throwable{
     @Override
     public String getMessage(){
-        return "Delete Delivery Does not exist Employee in Database";
+        return "Employee not exist Employee in Database";
     }
 }
