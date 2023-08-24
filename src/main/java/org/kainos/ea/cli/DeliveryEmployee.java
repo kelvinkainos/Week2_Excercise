18e2e5b3bbc7d8ccbd63c328d0f4a10301e1a78c
SeanMcAvoy29 <sean.mcavoy@Kainos.com>
2023-08-24 11:54:24 +0100

    DeliveryEmployee created

---
+++ src/main/java/org/kainos/ea/cli/DeliveryEmployee.java
@@ -0,0 +1,7 @@
+package org.kainos.ea.cli;
+
+public class DeliveryEmployee extends Employee{
+    public DeliveryEmployee(int employeeId, String name, double salary, String bankAccNum, String nationalInsuranceNum) {
+        super(employeeId, name, salary, bankAccNum, nationalInsuranceNum);
+    }
+}
