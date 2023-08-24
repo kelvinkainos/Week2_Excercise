5972f9132d87e893a4f9b988ef96c616141d07a4
SeanMcAvoy29 <sean.mcavoy@Kainos.com>
2023-08-24 14:33:29 +0100

    EmployeeDoesNoteExistException created

--- src/main/java/org/kainos/ea/api/EmployeeService.java
+++ src/main/java/org/kainos/ea/api/EmployeeService.java
@@ -8,5 +8,22 @@ import java.sql.SQLException;
 public class EmployeeService {
     private EmployeeDao employeeDao = new EmployeeDao();
 
+    public void deleteEmployee(int id) throws EmployeeDoesNoteExistException, FailedToDeleteProductException{
+        try {
+            Product productToDelete = productDao.getProductsById(id);
+
+            if(productToDelete == null){
+                throw new ProductDoesNoteExistException();
+            }
+
+            productDao.deleteProduct(id);
+        }catch (SQLException e){
+            System.err.println(e.getMessage());
+            throw new FailedToDeleteProductException();
+        }
+    }
+
+
+
 
 }
