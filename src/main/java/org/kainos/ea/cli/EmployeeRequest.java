086895bdc0fe0746e056b8782cbc50b3b1e6589d
SeanMcAvoy29 <sean.mcavoy@Kainos.com>
2023-08-24 12:03:47 +0100

    EmployeeRequest added constructor, getters and Setters

--- src/main/java/org/kainos/ea/cli/EmployeeRequest.java
+++ src/main/java/org/kainos/ea/cli/EmployeeRequest.java
@@ -1,4 +1,47 @@
 package org.kainos.ea.cli;
 
 public class EmployeeRequest {
+    private String name;
+    private double salary;
+    private String bankAccNum;
+    private String nationalInsuranceNum;
+
+    public EmployeeRequest(String name, double salary, String bankAccNum, String nationalInsuranceNum) {
+        this.name = name;
+        this.salary = salary;
+        this.bankAccNum = bankAccNum;
+        this.nationalInsuranceNum = nationalInsuranceNum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getBankAccNum() {
+        return bankAccNum;
+    }
+
+    public void setBankAccNum(String bankAccNum) {
+        this.bankAccNum = bankAccNum;
+    }
+
+    public String getNationalInsuranceNum() {
+        return nationalInsuranceNum;
+    }
+
+    public void setNationalInsuranceNum(String nationalInsuranceNum) {
+        this.nationalInsuranceNum = nationalInsuranceNum;
+    }
 }
