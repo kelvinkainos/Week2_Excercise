fca2c550b866f1a49370373f940a31e1b03dbb01
SeanMcAvoy29 <sean.mcavoy@Kainos.com>
2023-08-24 11:32:09 +0100

    Employee Class created

---
+++ src/main/java/org/kainos/ea/cli/Employee.java
@@ -0,0 +1,10 @@
+package org.kainos.ea.cli;
+
+public class Employee {
+    private int employeeId;
+    private String name;
+    private double salary;
+    private String bankAccNum;
+    private String nationalInsuranceNum;
+
+}
