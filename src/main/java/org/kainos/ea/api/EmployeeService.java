e757afe13808981fb9c5e7e410bb4100f676d9bd
SeanMcAvoy29 <sean.mcavoy@Kainos.com>
2023-08-24 14:11:19 +0100

    EmployeeService.java created

--- src/main/java/org/kainos/ea/api/EmployeeService.java
+++ src/main/java/org/kainos/ea/api/EmployeeService.java
@@ -1,4 +1,12 @@
 package org.kainos.ea.api;
 
+import org.kainos.ea.cli.EmployeeRequest;
+import org.kainos.ea.db.EmployeeDao;
+
+import java.sql.SQLException;
+
 public class EmployeeService {
+    private EmployeeDao employeeDao = new EmployeeDao();
+
+
 }
